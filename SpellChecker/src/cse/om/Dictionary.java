e42a34e80619ae476a39040fed6eab4bf7f8b80d
hungle.info <hungle.info@gmail.com>
2013-10-21 21:01:42 +0700

    init

---
+++ SpellChecker/src/cse/om/Dictionary.java
@@ -0,0 +1,14 @@
+package cse.om;
+
+public class Dictionary {
+
+	public Dictionary() {
+		System.out.println("dfgdfg");
+	}
+	
+	public String getDefination(String word) {
+		String def = "";
+		
+		return def;
+	}
+}
---
+++ SpellChecker/src/cse/test/Test.java
@@ -0,0 +1,12 @@
+package cse.test;
+
+import cse.om.Dictionary;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Dictionary dict = new Dictionary();
+		dict.getDefination("SDFG");
+	}
+
+}
