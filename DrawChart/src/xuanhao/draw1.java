0d5e8fa8f54a87ba593e07fd3655b18c75595509
xuanhao <mrtea91@gmail.com>
2013-11-07 20:59:34 +0700

    ve bieu do mien

--- DrawChart/src/xuanhao/draw1.java
+++ DrawChart/src/xuanhao/draw1.java
@@ -1,13 +1,144 @@
 package xuanhao;
 
-public class draw1 {
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.StackedBarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class draw1 extends ApplicationFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @param args
 	 */
+	
+	 /**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     */
+    public draw1(String title) {
+        super(title);
+        JPanel chartPanel = createDemoPanel();
+        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 600));
+        setContentPane(chartPanel);
+    }
+
+    /**
+     * Creates a sample dataset.
+     *
+     * @return a sample dataset.
+     */
+    private static CategoryDataset createDataset() {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(32.4, "Series 1", "Category 1");
+        dataset.addValue(17.8, "Series 2", "Category 1");
+        dataset.addValue(27.7, "Series 3", "Category 1");
+        dataset.addValue(43.2, "Series 1", "Category 2");
+        dataset.addValue(15.6, "Series 2", "Category 2");
+        dataset.addValue(18.3, "Series 3", "Category 2");
+        dataset.addValue(23.0, "Series 1", "Category 3");
+        dataset.addValue(11.3, "Series 2", "Category 3");
+        dataset.addValue(25.5, "Series 3", "Category 3");
+        dataset.addValue(13.0, "Series 1", "Category 4");
+        dataset.addValue(11.8, "Series 2", "Category 4");
+        dataset.addValue(29.5, "Series 3", "Category 4");
+        dataset.addValue(13.0, "Series 1", "Category 5");
+        dataset.addValue(11.8, "Series 2", "Category 5");
+        dataset.addValue(29.5, "Series 3", "Category 5");
+        return dataset;
+    }
+    /**
+     * Creates a sample chart.
+     *
+     * @param dataset  the dataset for the chart.
+     *
+     * @return a sample chart.
+     */
+    private static JFreeChart createChart(CategoryDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createStackedBarChart(
+            "Stacked Bar Chart Demo 7",  // chart title
+            "Category",                  // domain axis label
+            "Value",                     // range axis label
+            dataset,                     // data
+            PlotOrientation.VERTICAL,    // the plot orientation
+            true,                        // legend
+            true,                        // tooltips
+            false                        // urls
+        );
+
+        CategoryPlot plot = (CategoryPlot) chart.getPlot();
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setNumberFormatOverride(NumberFormat.getPercentInstance());
+        StackedBarRenderer renderer = (StackedBarRenderer) plot.getRenderer();
+        renderer.setRenderAsPercentages(true);
+        renderer.setDrawBarOutline(false);
+        renderer.setBaseItemLabelsVisible(true);
+        renderer.setBaseItemLabelGenerator(
+                new StandardCategoryItemLabelGenerator());
+        
+        chart.setBackgroundPaint(Color.white);
+		plot.setBackgroundPaint(Color.white);
+		plot.setRangeGridlinePaint(Color.blue);
+		chart.setAntiAlias(true);
+        
+        try {
+			ChartUtilities.saveChartAsJPEG(new File("src/chart1.jpg"), chart, 1000, 600);
+			} catch (IOException e) {
+			System.err.println("Problem occurred creating chart.");
+			}
+             
+		
+//		BarRenderer renderer = (BarRenderer) plot.getRenderer();
+//		renderer.setSeriesPaint(0, Color.CYAN);
+//		renderer.setSeriesPaint(1, Color.green);
+//		renderer.setDrawBarOutline(false);
+        
+        return chart;
+
+    }
+
+    /**
+     * Creates a panel for the demo (used by SuperDemo.java).
+     *
+     * @return A panel.
+     */
+    public static JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());        
+        return new ChartPanel(chart);
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		draw1 demo = new draw1(
+                "Stacked Bar Chart Demo 7");
+		
+		
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
 	}
 
 }
